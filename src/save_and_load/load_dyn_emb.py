bd104ff8cc828394b2a12788b85afc106c525cad
nov11 <529079634@qq.com>
2021-12-26 15:45:55 +0800

    change weight and save

--- src/save_and_load/load_dyn_emb.py
+++ src/save_and_load/load_dyn_emb.py
@@ -1,3 +1,6 @@
+import logging
+import shutil
+
 import numpy as np
 import tensorflow._api.v2.compat.v1 as tf
 
@@ -5,9 +8,14 @@ import export_paths
 import inner_export_utils
 import simple_model
 
-# import tensorflow_recommenders_addons.dynamic_embedding as de
+import tensorflow_recommenders_addons.dynamic_embedding as de
 
 tf.disable_eager_execution()
+
+load_dir = export_paths.dyn_emb_dir
+output_dir = export_paths.dyn_emb_out_dir
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     # not working :KeyError: "The name 'A' refers to an Operation not in the graph."
@@ -16,9 +24,38 @@ if __name__ == '__main__':
     #                                                 export_paths.dyn_emb_dir)
     #     inner_export_utils.show_op_names()
 
+    new_key = np.arange(20, 30)
+    new_val = np.tile(np.arange(0, 8), [10, 1])
     with tf.Session(graph=tf.Graph()) as sess:
         pa, pb, o = simple_model.make_model_dyn_emb()
         saver = tf.train.Saver()
-        saver.restore(sess, export_paths.dyn_emb_dir + '/ckpt')
-        print(sess.run(o, feed_dict={pa: np.ones([1, 512]), pb: np.ones([1, 512])}))
-        inner_export_utils.show_op_names()
+        saver.restore(sess, load_dir + '/ckpt')
+        logger.info(sess.run(o, feed_dict={pa: np.ones([1, 512]), pb: np.ones([1, 512])}))
+        # inner_export_utils.show_op_names()
+        # this is not applicable
+        # w_a = sess.run('A:0')
+        # print(w_a)
+
+        logger.info("assign weights")
+        with tf.variable_scope(name_or_scope='', reuse=True):
+            v: de.Variable = de.get_variable('A')
+            logger.info(sess.run(v.export("export_values0")))
+
+            sess.run(v.upsert(new_key, new_val, name='upsert'))
+            logger.info(sess.run(v.export("export_values1")))
+
+        shutil.rmtree(output_dir, ignore_errors=True)
+        inner_export_utils.do_save_model(output_dir, sess, pa, pb, o)
+        inner_export_utils.write_graph(output_dir, sess.graph)
+        saver = tf.train.Saver()
+        saver.save(sess, output_dir + '/ckpt')
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        pa, pb, o = simple_model.make_model_dyn_emb()
+        saver = tf.train.Saver()
+        saver.restore(sess, output_dir + '/ckpt')
+        logger.info(sess.run(o, feed_dict={pa: np.ones([1, 512]), pb: np.ones([1, 512])}))
+
+        with tf.variable_scope(name_or_scope='', reuse=True):
+            v: de.Variable = de.get_variable('A')
+            logger.info(sess.run(v.export("export_values0")))
--- src/save_and_load/simple_model.py
+++ src/save_and_load/simple_model.py
@@ -27,17 +27,19 @@ def make_target_model():
 
 def make_model_dyn_emb():
     a = de.get_variable(name='A', key_dtype=tf.int32, value_dtype=tf.int32, dim=8)
-    b = tf.Variable(initial_value=np.ones([512, 8]), name='B', dtype=tf.int32)
+    b = tf.Variable(initial_value=np.zeros([512, 8]), name='B', dtype=tf.int32)
     pa = tf.placeholder(dtype=tf.int32, shape=[None, 512], name='pa')
     pb = tf.placeholder(dtype=tf.int32, shape=[None, 512], name='pb')
 
     sp = tf.sparse.from_dense(pa)
 
     ma = de.embedding_lookup_sparse(a,
+                                    tf.SparseTensor(indices=sp.indices,
+                                                    values=tf.cast(sp.indices[:, 1], dtype=tf.int32),
+                                                    dense_shape=sp.dense_shape),
                                     tf.SparseTensor(indices=sp.indices, values=sp.values, dense_shape=sp.dense_shape),
-                                    tf.SparseTensor(indices=sp.indices, values=sp.values, dense_shape=sp.dense_shape),
+                                    combiner='sum'
                                     )
-    print('ma:', ma)
     mb = tf.matmul(pb, b)
     add_op = tf.add(ma, mb, name='Add')
 
